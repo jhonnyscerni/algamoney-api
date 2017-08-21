496b1b6c66b0a1b3a6e77cf1b896b5af0f43708c
jhonnyscerni@gmail.com <jhonnyscerni@gmail.com>
2017-08-20 23:23:44 -0300

    Removendo pessoa com DELETE - Simples

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/PessoaResource.java
@@ -8,11 +8,13 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.algaworks.algamoneyapi.event.RecursoCriadoEvent;
 import com.algaworks.algamoneyapi.model.Pessoa;
@@ -43,6 +45,12 @@ public class PessoaResource {
 		Pessoa pessoa = pessoaRespository.findOne(codigo);
 		
 		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build(); 
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo){
+		pessoaRespository.delete(codigo);
 	}
 	
 }
