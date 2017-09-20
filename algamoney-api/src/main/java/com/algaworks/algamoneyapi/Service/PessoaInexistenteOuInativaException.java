b04519c20b662fa3951dbdd845b49474fb399c1a
jhonnyscerni <jhonnyscerni@gmail.com>
2017-09-20 14:56:59 -0300

    Regra para não salvar pessoa inativa

---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/Service/PessoaInexistenteOuInativaException.java
@@ -0,0 +1,13 @@
+package com.algaworks.algamoneyapi.Service;
+
+public class PessoaInexistenteOuInativaException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	
+}
