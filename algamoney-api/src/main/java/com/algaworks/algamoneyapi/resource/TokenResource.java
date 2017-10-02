7b75810b0ed7e821f9330cb28446f2bb4acfc211
jhonnyscerni@gmail.com <jhonnyscerni@gmail.com>
2017-10-02 16:06:44 -0300

    Profiles do Spring

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/TokenResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/TokenResource.java
@@ -3,20 +3,26 @@ package com.algaworks.algamoneyapi.resource;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algamoneyapi.config.property.AlgaMoneyApiProperty;
+
 @RestController
 @RequestMapping("/tokens")
 public class TokenResource {
+	
+	@Autowired
+	private AlgaMoneyApiProperty algaMoneyApiProperty;
 
 	@DeleteMapping("/revoke")
 	public void revoke(HttpServletResponse req, HttpServletResponse resp){
 		Cookie cookie = new Cookie("refreshToken", null);
 		cookie.setHttpOnly(true);
-		cookie.setSecure(false); // TODO: Em produção sera true
+		cookie.setSecure(algaMoneyApiProperty.getSeguranca().isEnableHttps());
 		cookie.setPath(req.getContentType() + "/oauth/token");
 		cookie.setMaxAge(0);
 		
